f6ac5d87070a7c0b45835e40916825d445648262
franknstin <anmhatre94@gmail.com>
2022-09-23 17:07:24 +0530

    updated

--- src/deepClassifier/__init__.py
+++ src/deepClassifier/__init__.py
@@ -2,13 +2,13 @@ import os
 import sys
 import logging
 
-logging_str = "[%(acstime)s: %(levelname)s: %(module)s]: %(message)s"
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s]: %(message)s"
 log_dir = 'logs'
 log_filepath = os.path.join(log_dir, 'running_logs.log')
 os.makedirs(log_dir, exist_ok=True)
 
 logging.basicConfig(
-    Level=logging.INFO,
+    level=logging.INFO,
     format=logging_str,
     handlers=[
         logging.FileHandler(log_filepath),
